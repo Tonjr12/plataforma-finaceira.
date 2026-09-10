973fc9723c5e9a39fd986b137a276a0214628352
Tonjr <wwffjjr@gmail.com>
2026-09-10 16:51:55 -0300

    feat: conecta gerador de dados ao script principal main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,13 @@
+from ingestion.gerador_transacoes import gerar_transacao
+
 def main():
     print("=== Plataforma de Análise Financeira e Risco ===")
-    print("Status: Sistema inicializado com sucesso.")
+    print("Status: Sistema inicializado com sucesso.\n")
+    
+    print("--- Simulando Ingestão de Transações ---")
+    for i in range(1, 6):
+        transacao = gerar_transacao()
+        print(f"Transação {i}: {transacao}")
 
 if __name__ == "__main__":
     main()
